06a8254c2a4ee1027181671eee15704426a16e37
StafWeb <staf.target@yandex.ru>
2022-06-24 04:14:38 +0700

    del swiper.js

--- js/main.js
+++ js/main.js
@@ -1,36 +1,11 @@
-
-const swiper = new Swiper('.swiper', {
-  direction: "horizontal",
-  speed: 1500,
-  slidesPerGroup: 2,
-
-  // parallax: true,
-  // centeredSlides: true,
-  slidesPerView: 3,
-  // spaceBetween: 30,
-  // centeredSlidesBounds:true,
-  passiveListeners: true,
-  mousewheel: {
-    releaseOnEdges: true,
-    sensitivity: 0.7,
-    // thresholdTime: 100,
-    // thresholdDelta: 0.3,
-  },
-  pagination: {
-    el: '.swiper-pagination',
-    type: 'progressbar',
-    // clickable: true,
-  },
-});
-
 let options = {
   threshold: [0]
 };
 let observer = new IntersectionObserver(onEntry, options);
 // let images = document.querySelectorAll('[data-img]');
-// let blockInfo = document.querySelector('.swiper-block__info');
+let blockInfo = document.querySelector('.swiper-block__info');
 let swiperDescr = document.querySelectorAll('.swiper-slide')
-// observer.observe(blockInfo);
+observer.observe(blockInfo);
 // for (let img of images) {
 //   observer.observe(img);
 // };
@@ -44,3 +19,58 @@ function onEntry(entry) {
     }
   });
 };
+
+
+// const swiper = new Swiper('.swiper', {
+//   direction: "horizontal",
+//   speed: 900,
+//   slidesPerGroup: 2,
+//   preloadImages: true,
+//   slidesPerView: 'auto',
+//   spaceBetween: 30,
+//   passiveListeners: true,
+//   mousewheel: {
+//     releaseOnEdges: true,
+//     // sensitivity: 1,
+//     // thresholdTime: 100,
+//     // thresholdDelta: 0.3,
+//   },
+//   pagination: {
+//     el: '.swiper-pagination',
+//     type: 'progressbar',
+//     // clickable: true,
+//   },
+// });
+
+(function() {
+
+  function scrollHorizontally(e) {
+
+    var scrollPos = this.scrollLeft;  // Сколько прокручено по горизонтали, до прокрутки (не перемещать эту строку!)
+
+    // Горизонтальная прокрутка
+    e = window.event || e;
+    var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
+    this.scrollLeft -= (delta*10); // Multiplied by 10
+
+    var widthElem = this.scrollWidth; // Ширина всего элемента
+    var widthBrowser = document.documentElement.clientWidth;  // Ширина окна минус размер вертикального скролла
+
+
+    // Прокрутка вверх, но элемент уже в крайней левой позиции, то двигаемся вверх
+    if ((delta == 1 ) && (this.scrollLeft == 0)) return;
+    // Прокрутка вниз, но элемент виден полностью. Или элемент до конца прокручен в правый край
+    if ((widthBrowser >= widthElem) || (scrollPos == this.scrollLeft)) return;
+
+    e.preventDefault(); // запрещает прокрутку по вертикали
+
+  }
+
+
+  var elems = document.querySelectorAll('.swiper-wrapper');
+  for (var a = 0; a < elems.length; a++) {
+    elems[a].addEventListener("mousewheel", scrollHorizontally, false);     // IE9, Chrome, Safari, Opera
+    elems[a].addEventListener("DOMMouseScroll", scrollHorizontally, false); // Firefox
+  }
+
+})();
